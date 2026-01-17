bb9ad9c2442372c3c0590e6f67d7f45a326ec35c
ADIIN11 <adithyaaravind1105@gmail.com>
2026-01-18 03:56:12 +0530

    installed express and made server functional

--- Backend_server.js
+++ Backend_server.js
@@ -1,16 +1,79 @@
-const http=require("http")
-const server=http.createServer((req,res)=>{
-    res.write("hello world, this is a webserver")
-    res.end()
-})
-server.listen(5000)
+// const http=require("http")
+// const fs=require("fs")
+// const path=require("path")
+
+// const server=http.createServer((req,res)=>{
+//     if(req.url==='/'){
+//         const filePath= path.join(__dirname,"store.html")
+
+//         fs.readFile(filePath,(err,data)=>{
+//             if(err){
+//                 res.writeHead(500,{"content-type":"text/plain"})
+//                 res.end("Internal Error")
+//             }
+//             else{
+//                 res.writeHead(200,{"content-type":"text/html"})
+//                 res.end(data)
+//             }
+//         })
+//     }
+//     else if(req.url==='/Sign_Up'){
+//         const filePath= path.join(__dirname,"Sign_Up.html")
+//           fs.readFile(filePath,(err,data)=>{
+//             if(err){
+//                 res.writeHead(500,{"content-type":"text/plain"})
+//                 res.end("Internal Error")
+//             }
+//             else{
+//                 res.writeHead(200,{"content-type":"text/html"})
+//                 res.end(data)
+//             }
+//         })
+
+//     }
+//     else{
+//         res.writeHead(404, { 'Content-Type': 'text/plain' })
+//         res.end('Page Not Found')
+//     }
+// })
+// server.listen(5000,()=>{
+//     console.log("listening to port 5000")
+// })
+
+
+
+//access server by http://localhost:5000
 
 
 
 
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 5000;
 
 
+app.use(express.static(path.join(__dirname, "public")));
 
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "store.html"));
+});
+
+
+app.get("/Sign_Up", (req, res) => {
+  res.sendFile(path.join(__dirname, "Sign_Up.html"));
+});
+
+
+app.use((req, res) => {
+  res.status(404).send("Page Not Found");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
 
 
 
