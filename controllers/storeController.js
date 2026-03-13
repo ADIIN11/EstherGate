653f2beb7deb939fd12542dda576a051b2344c50
ADIIN11 <adithyaaravind1105@gmail.com>
2026-03-13 23:27:39 +0530

    working on backend

---
+++ controllers/storeController.js
@@ -0,0 +1,29 @@
+
+exports.getTopSellingProducts=async(req,res)=>{
+  try{
+    const limit = 10
+
+    const skipAmount = (req.body.page - 1) * limit
+    const topSellingProducts = await productModel.find({}, { productId: 1, name: 1, productImg1: 1, currency: 1, price: 1,discount: 1, _id: 0 }).sort({ productBought: -1 }).skip(skipAmount).limit(limit)
+    res.json({ topSellingProducts:topSellingProducts
+       })
+
+  }catch(err){
+    console.log("error while fetching topSellingProducts:",err)
+  }
+
+}
+
+exports.getMostViewedProducts=async(req,res)=>{
+  try{
+    const limit = 10
+    const skipAmount = (req.body.page - 1) * limit
+    const mostViewedProducts = await productModel.find({}, { productId: 1, name: 1, productImg1: 1, currency: 1, price: 1,discount: 1, _id: 0 }).sort({ views: -1 }).skip(skipAmount).limit(10)
+    res.json({ mostViewedProducts:mostViewedProducts
+       })
+
+  }catch(err){
+    console.log("error while fetching mostViewedProducts:",err)
+  }
+
+}
