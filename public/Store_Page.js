e43f4ecc0215d1c83aec70c41d45284effe5c034
ADIIN11 <adithyaaravind1105@gmail.com>
2026-03-18 23:24:56 +0530

    working on storepage backend

--- public/Store_Page.js
+++ public/Store_Page.js
@@ -162,6 +162,12 @@ async function signOut(){
   
 }
 
+const urlParts = window.location.pathname.split("/"); 
+const productId = urlParts[urlParts.length - 1]
+
+
+
+
 let topSellingProducts = []
 let topSellingProductsRow=""
 let currentTopSellingPage = 1
